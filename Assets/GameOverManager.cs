e2f981431b207ac5feb2413b001dfc0cfb95d3a4
Tülin Eren <erentulin5@gmail.com>
2025-12-13 21:21:49 +0300

    1

--- Assets/GameOverManager.cs
+++ Assets/GameOverManager.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameOverManager
+public class GameOverManager : MonoBehaviour
 {
-    
+    // Ekrana týklayýnca çalýþacak
+    public void OyunuBasaSar()
+    {
+        // 0 numaralý sahneye (Main_Scene / Menü) döner
+        SceneManager.LoadScene(0);
+    }
 }
